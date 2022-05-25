9e1328c2a20537466e5603d477787a40d36db1e9
Mahesh kumar <jayaprakashrao99@gmail.com>
2022-05-25 16:36:21 +0530

    new changes

--- first/src/first/hello_world.java
+++ first/src/first/hello_world.java
@@ -4,6 +4,6 @@ public class hello_world
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("This is my first github project to push!!!!!");
+		System.out.println("This is my first github project to push");
 	}
 }
